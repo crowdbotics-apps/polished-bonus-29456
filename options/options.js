17cfbc346e15a2745661b9b0503f32e315820413
Crowdbotics <team@crowdbotics.com>
2021-08-04 16:07:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "polished_bonus_29456Identifier",
+  url: "https://polished_bonus_29456Identifier.botics.co",
+  api: "https://polished_bonus_29456Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
